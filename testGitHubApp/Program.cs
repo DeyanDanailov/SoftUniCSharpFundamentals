05361b502e1b2d8f8f9f2df344802c7a85db85b6
DeyanDanailov <stormbringer_ss@abv.bg>
2020-07-29 17:37:19 +0300

    Initial code added

---
+++ testGitHubApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testGitHubApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
