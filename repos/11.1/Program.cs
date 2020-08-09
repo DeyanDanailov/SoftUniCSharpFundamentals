1e68c57adf16adfe469c50d3ca64f54029fb2448
DeyanDanailov <stormbringer_ss@abv.bg>
2020-08-09 17:53:11 +0300

    test

---
+++ repos/11.1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _11._1.Exam1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string input = Console.ReadLine();
+            while (input != "Travel")
+            {
+                string[] command = input.Split(':', StringSplitOptions.RemoveEmptyEntries);
+                string action = command[0];
+                if (action == "Add Stop")
+                {
+                    int index = int.Parse(command[1]);
+                    if (index >= 0 && index < text.Length)
+                    {
+                        text = text.Insert(index, command[2]);                      
+                    }
+                    
+                } 
+                else if (action == "Remove Stop")
+                {
+                    int startIndex = int.Parse(command[1]);
+                    int endIndex = int.Parse(command[2]);
+                    if (startIndex>=0 && startIndex <= endIndex && endIndex < text.Length)
+                    {
+                        int length = endIndex - startIndex + 1;
+                        text = text.Remove(startIndex, length);                       
+                    }
+                   
+                }
+                else if (action == "Switch")
+                {
+                    string oldString = command[1];
+                    string newString = command[2];
+                    if (text.Contains(oldString))
+                    {
+                        text = text.Replace(oldString, newString);
+                        //Console.WriteLine(text);
+                    }
+                    
+                }
+                Console.WriteLine(text);
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Ready for world tour! Planned stops: {text}");
+        }
+    }
+}
