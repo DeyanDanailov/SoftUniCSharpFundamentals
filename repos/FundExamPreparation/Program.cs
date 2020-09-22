1973d65c49995520b1095d51f188ce0967d5c55b
DeyanDanailov <stormbringer_ss@abv.bg>
2020-09-22 14:27:09 +0300

    test

--- repos/FundExamPreparation/Program.cs
+++ repos/FundExamPreparation/Program.cs
@@ -6,7 +6,7 @@ namespace FundExamPreparation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, komandir Koko!");
         }
     }
 }
